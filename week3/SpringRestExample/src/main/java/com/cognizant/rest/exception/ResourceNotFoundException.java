2931e1a36e5e8146c1af478e629f6daf0af4b045
Jennita-Santhakumar <160242777+Jennita-Santhakumar@users.noreply.github.com>
2026-07-23 11:42:56 +0530

    Delete week3/SpringRestExample directory

--- week3/SpringRestExample/src/main/java/com/cognizant/rest/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.rest.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- week3/SpringRestExample/src/main/java/com/cognizant/rest/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.rest.repository;
-
-import com.cognizant.rest.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
